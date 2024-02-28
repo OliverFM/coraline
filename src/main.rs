91a3a83c2d0d0e43a0c6ce291cc50bf6ba4fe0cf
Oliver Makins <oliver@pactio.io>
2024-02-28 09:33:46 +0000

    cleaned up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use reqwest::Client;
 use serde_json::json;
-use std::fs::File;
 use std::io::copy;
 
 use clap::Parser;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -14,15 +12,20 @@ struct Args {
 
     #[arg(short, long)]
     output_file: String,
+
+    #[arg(long, default_value_t = String::from("alloy"))]
+    voice: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
     let api_url = "https://api.openai.com/v1/audio/speech";
-    let api_key = std::env::var("OPENAI_API_KEY").expect("you should have an api key"); // Replace with your actual API key
+    let api_key = std::env::var("OPENAI_API_KEY")
+        .expect("\nYou need to have OPENAI_API_KEY present in your env.\n");
     let args = Args::parse();
 
+    let mut dest = std::fs::File::create(&args.output_file)?;
     let input = std::fs::read_to_string(&args.input_file)?;
 
     let response = client
@@ -33,14 +36,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             json!({
                 "model": "tts-1",
                 "input": input,
-                "voice": "alloy"
+                "voice": args.voice,
             })
             .to_string(),
         )
         .send()
         .await?;
 
-    let mut dest = { std::fs::File::create(&args.output_file)? };
     let content = response.bytes().await?;
     copy(&mut content.as_ref(), &mut dest)?;
 
