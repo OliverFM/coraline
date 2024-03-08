d40864880ea04d78a7aea29cb46e378ae1d81db2
Oliver Makins <oliver@pactio.io>
2024-03-08 17:32:19 +0000

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
 use clap::{Parser, Subcommand};
 use env_logger::Env;
 use log;
-use reqwest::Client;
-use serde::Serialize;
-use serde_json::json;
-use std::io::copy;
 use tokio;
 
-#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
-#[serde(rename_all = "kebab-case")]
-enum Voice {
-    #[default]
-    Alloy,
-    Echo,
-    Fable,
-    Onyx,
-    Nova,
-    Shimmer,
-}
+mod listen;
+mod text_to_speech;
+
+use text_to_speech::{tts, Voice};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -78,101 +67,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             tts(voice, &args.input_file, &args.output_file, &api_key).await?;
         }
         Commands::Listen => {
-            listen(&args.input_file, &args.output_file, &api_key).await?;
+            listen::listen(&args.input_file, &args.output_file, &api_key).await?;
         }
     }
 
-    Ok(())
-}
-
-async fn listen(
-    input_file: &str,
-    output_file: &str,
-    api_key: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
-    let api_url = "https://api.openai.com/v1/audio/transcriptions";
-
-    let audio_file: tokio::fs::File = match tokio::fs::File::open(input_file).await {
-        Ok(f) => f,
-        Err(e) => {
-            log::error!("Oh no! Could not read your audio file: {}", input_file);
-            return Err(e.into());
-        }
-    };
-
-    let mime_type = mime_guess::from_path(input_file).first_or_octet_stream();
-    let form = reqwest::multipart::Form::new()
-        .text("model", "whisper-1")
-        .text("response_format", "text")
-        .part(
-            "file",
-            reqwest::multipart::Part::stream(audio_file)
-                .file_name("file")
-                .mime_str(&mime_type.to_string())?,
-        )
-        .text("model", "whisper-1");
-    log::info!("Sending request to OpenAI's API...");
-    let response = client
-        .post(api_url)
-        .header("Authorization", format!("Bearer {}", api_key))
-        .multipart(form)
-        .send()
-        .await?;
-
-    let status = response.status();
-    log::info!("Response status: {}", status);
-    if status.is_client_error() || status.is_server_error() {
-        let error = response.text().await?;
-        log::error!("Error: {}", error);
-        return Err("Error from OpenAI's API".into());
-    }
-    let content = response.bytes().await?;
-
-    let mut dest = std::fs::File::create(output_file)?;
-    copy(&mut content.as_ref(), &mut dest)?;
-    log::info!("Successfully saved the text to: {}", output_file);
-    Ok(())
-}
-
-async fn tts(
-    voice: Voice,
-    input_file: &str,
-    output_file: &str,
-    api_key: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
-    let api_url = "https://api.openai.com/v1/audio/speech";
-    let mut dest = std::fs::File::create(output_file)?;
-    let input = std::fs::read_to_string(input_file)?;
-
-    log::info!("Voice is: {:?}", voice);
-    let body = json!({
-        "model": "tts-1",
-        "input": input,
-        "voice": voice,
-    })
-    .to_string();
-    log::debug!("Body is:\n{}", body);
-    log::info!("Sending request to OpenAI's API...");
-    let response = client
-        .post(api_url)
-        .header("Authorization", format!("Bearer {}", api_key))
-        .header("Content-Type", "application/json")
-        .body(body)
-        .send()
-        .await?;
-
-    let status = response.status();
-    log::info!("Response status: {}", status);
-    if status.is_client_error() || status.is_server_error() {
-        let error = response.text().await?;
-        log::error!("Error: {}", error);
-        return Err("Error from OpenAI's API".into());
-    }
-    let content = response.bytes().await?;
-    copy(&mut content.as_ref(), &mut dest)?;
-    log::info!("Successfully saved the audio to: {}", output_file);
     Ok(())
 }
 
@@ -186,7 +84,7 @@ mod tests {
         tts(Voice::Alloy, "sample.txt", "intermediate.mp3", api_key)
             .await
             .unwrap();
-        listen("intermediate.mp3", "testing_output.txt", api_key)
+        listen::listen("intermediate.mp3", "testing_output.txt", api_key)
             .await
             .unwrap();
         std::fs::remove_file("intermediate.mp3").unwrap();
---
+++ src/text_to_speech.rs
@@ -0,0 +1,61 @@
+use clap::{Parser, Subcommand};
+use env_logger::Env;
+use log;
+use reqwest::Client;
+use serde::Serialize;
+use serde_json::json;
+use std::io::copy;
+use tokio;
+
+#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum Voice {
+    #[default]
+    Alloy,
+    Echo,
+    Fable,
+    Onyx,
+    Nova,
+    Shimmer,
+}
+
+pub async fn tts(
+    voice: Voice,
+    input_file: &str,
+    output_file: &str,
+    api_key: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let api_url = "https://api.openai.com/v1/audio/speech";
+    let mut dest = std::fs::File::create(output_file)?;
+    let input = std::fs::read_to_string(input_file)?;
+
+    log::info!("Voice is: {:?}", voice);
+    let body = json!({
+        "model": "tts-1",
+        "input": input,
+        "voice": voice,
+    })
+    .to_string();
+    log::debug!("Body is:\n{}", body);
+    log::info!("Sending request to OpenAI's API...");
+    let response = client
+        .post(api_url)
+        .header("Authorization", format!("Bearer {}", api_key))
+        .header("Content-Type", "application/json")
+        .body(body)
+        .send()
+        .await?;
+
+    let status = response.status();
+    log::info!("Response status: {}", status);
+    if status.is_client_error() || status.is_server_error() {
+        let error = response.text().await?;
+        log::error!("Error: {}", error);
+        return Err("Error from OpenAI's API".into());
+    }
+    let content = response.bytes().await?;
+    copy(&mut content.as_ref(), &mut dest)?;
+    log::info!("Successfully saved the audio to: {}", output_file);
+    Ok(())
+}
